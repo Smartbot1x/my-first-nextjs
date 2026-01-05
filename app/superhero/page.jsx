39ccf233a39ec3420dbf9bebea5ca5fde0858fc3
mohamed Osman <smartbot616@gmail.com>
2026-01-05 11:12:38 +0100

    some extra stuffs

---
+++ app/superhero/page.jsx
@@ -0,0 +1,16 @@
+const Page = () => {
+  return (
+    <div>
+      <main>
+        <h1 className="text-center font-bold text-3xl text-sky-600 mt-5 ">
+          Velkommen til Superhero HQ
+        </h1>
+        <p className="text-center">
+          Du har nu lavet din første egen Next.js-side!
+        </p>
+      </main>
+    </div>
+  );
+};
+
+export default Page;
